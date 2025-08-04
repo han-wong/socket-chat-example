72464638d0e334fb35ed6316a31e79c34d91e8a6
Han Wong <han.wong@pm.me>
2025-08-04 08:51:47 +0200

    fix 3

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.use(cors());
